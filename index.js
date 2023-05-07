cabfc939ef14c614531a4e196bb753c065e5cff6
notdien <thedien7000@gmail.com>
2023-05-07 10:59:19 -0400

    created routes

--- index.js
+++ index.js
@@ -3,6 +3,25 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
 const ejs = require('ejs');
 app.set('view engine', 'ejs');
-app.set('pages', __dirname + '/pages');
+app.set('views', __dirname + '/views');
+
+
+// web server
+app.get('/', function(res) {
+    res.render('home')
+});
+
+app.get('/portfolio', function(res) {
+    res.render('portfolio')
+});
+
+app.get('/contact', function(res) {
+    res.render('contact')
+});
+
+app.listen(5678);
+console.log("Server is running...")
