de6551d3fd80616458293e69445a5114e62df35b
notdien <thedien7000@gmail.com>
2023-05-09 21:04:29 -0400

    started on endpoint for sending emails

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 const express = require('express');
 const app = express();
 
+const nodeMailer = require('nodemailer');
+app.use(express.json());
+
 // const bodyParser = require('body-parser');
+
+const config = require('./config');
 
 const ejs = require('ejs');
 app.set('view engine', 'ejs');
@@ -24,6 +29,18 @@ app.get('/portfolio', function(req, res) {
 app.get('/contact', function(req, res) {
     res.render('contact');
 });
+
+app.post('/send-email', async(req, res) => {
+    
+    var user_info = {
+        "name": req.body.name,
+        "number": req.body.number,
+        "email": req.body.email,
+        "message": req.body.email
+    }
+
+    
+})
 
 app.listen(5678);
 console.log("Server is running...")
