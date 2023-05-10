39e60950d6800a4503372c17e4d2b98acf8ab9d6
notdien <thedien7000@gmail.com>
2023-05-10 19:45:38 -0400

    got endpoint to somewhat function

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 const config = require('./config');
 
 const ejs = require('ejs');
+const { email } = require('./config');
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/views'));
@@ -37,10 +38,33 @@ app.post('/send-email', async(req, res) => {
         "number": req.body.number,
         "email": req.body.email,
         "message": req.body.email
-    }
+    };
 
-    
-})
+    let transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: config.email,
+            pass: config.password,
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: user_info.email,
+        to: 'dien7shoots@gmail.com',
+        subject: user_info.name,
+        text: user_info.message,
+    });
+
+    try {
+        await transporter.sendMail(info);
+        res.status(200).json({ message: 'Email sent successfully :)' });
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to send email :( '});
+    }
+});
 
 app.listen(5678);
 console.log("Server is running...")
